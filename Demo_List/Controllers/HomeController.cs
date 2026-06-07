9acc7d06e79931393df6d5916971ae481be39689
thienmai67 <lehongcuc19571952@gmail.com>
2026-06-07 18:27:16 +0700

    Add Create Staff feature

--- Demo_List/Controllers/HomeController.cs
+++ Demo_List/Controllers/HomeController.cs
@@ -1,20 +1,55 @@
 using Demo_List.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+
 namespace Demo_List.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<Staff> staffs = new List<Staff>()
+        {
+            new Staff
+            {
+                ID = "NV01",
+                FullName = "Mai Văn Minh Thiện",
+                NumberPhone = "037121238",
+                Email = "thienchupy@123",
+                Address = "An Giang"
+            },
+            new Staff
+            {
+                ID = "NV02",
+                FullName = "Hoàng Thành Nhân",
+                NumberPhone = "0112113123",
+                Email = "nhan@123",
+                Address = "Trà Vinh"
+            },
+            new Staff
+            {
+                ID = "NV03",
+                FullName = "Huỳnh Đức Thịnh",
+                NumberPhone = "0212312355",
+                Email = "thinh@123",
+                Address = "Sài Gòn"
+            }
+        };
+
         public IActionResult Index()
         {
-            List<Staff> nv = new List<Staff>()
-        {
-            new Staff {ID = "NV01", FullName = "Mai Văn Minh Thiện",NumberPhone = "037121238", Email = "thienchupy@123", Address = "An Giang"},
-            new Staff {ID = "NV02", FullName = "Hoàng Thành Nhân",NumberPhone = "0112113123", Email = "nhan@123", Address = "Trà Vinh"},
-            new Staff {ID = "NV03", FullName = "Huỳnh Đức Thịnh",NumberPhone = "0212312355", Email = "Thinh@123", Address = "Sài Gòn"},
-        };
-            return View(nv);
+            return View(staffs);
         }
-     
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Staff staff)
+        {
+            staffs.Add(staff);
+
+            return RedirectToAction("Index");
+        }
     }
 }
